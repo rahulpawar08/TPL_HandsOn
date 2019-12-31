34a71045d918c3a31a40a38b8dfb18f7859e3320
Rahul <rpawar@tavisca.com>
2019-12-31 11:32:34 +0530

    Implemented the Incremental Data Provider

---
+++ TPL.DataProviders/HotelResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TPL.DataProviders
+{
+    public class HotelResponse
+    {
+        public List<string> Hotels = new List<string>();
+        public int TickTime { get; set; }
+    }
+}
---
+++ TPL.DataProviders/IncrementalDataProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TPL.DataProviders
+{
+    public class IncrementalDataProvider
+    {
+        private const int BatchCount =30;
+
+        public HotelResponse GetHotels(string supplierName, int ticktime)
+        {
+            return GetIncrementalHotels(supplierName, ticktime);
+        }
+
+        private HotelResponse GetIncrementalHotels(string supplierName, int ticktime)
+        {
+            HotelResponse hotelResponse = new HotelResponse();
+            List<string> hotels = new List<string>();
+            int updatedTickTime = ticktime + 1;
+
+            int startIndex = ticktime * BatchCount;
+            int endIndex = startIndex + BatchCount;
+
+            for (int index = startIndex; index <= endIndex; index++)
+            {
+                string str = "hotel:" + index + "-Ticktime:" + updatedTickTime;
+                hotels.Add(str);
+            }
+            hotelResponse.Hotels = hotels;
+            hotelResponse.TickTime = updatedTickTime;
+
+            return hotelResponse;
+        }
+    }
+}
